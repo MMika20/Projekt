726af6832cdce5b25b99043d6084a3c24f5a7a2c
mika20 <mika20@schule.bremen.de>
2023-05-04 14:31:45 +0200

    04.05.2023 14:31

--- ProjektTaschenrechner/StatistischeAuswertungen.java
+++ ProjektTaschenrechner/StatistischeAuswertungen.java
@@ -1,4 +1,43 @@
 package ProjektTaschenrechner;
 
 public class StatistischeAuswertungen {
+
+    public static double arithMittel(double[] ArithArray){
+        double arithM = 0;
+        for (int i = 0; i < ArithArray.length; i++) {
+            arithM += (ArithArray[i]) / ArithArray.length;
+        }
+        return arithM;
+    }
+    /////////////////////////////////////////////////////////////////////////
+    public static double ermittelMax(double[] MaxArray) {
+        double max = 0;
+        for (int i = 0; i < MaxArray.length; i++) {
+            max = Math.max(max, MaxArray[i]);
+        }
+        return max;
+    }
+    /////////////////////////////////////////////////////////////////////////
+    public static double ermittelMin(double[] MinArray) {
+        double min = 0;
+        for (int i = 0; i < MinArray.length; i++) {
+            min = Math.min(min, MinArray[i]);
+        }
+        return min;
+    }
+    /////////////////////////////////////////////////////////////////////////
+    public static double varianz(double[] VarianzArray){
+        double var=0;
+        for(int i = 0; i<VarianzArray.length;i++){
+            var+= Math.pow(VarianzArray[i]-arithMittel(VarianzArray),2);
+        }
+        return var* VarianzArray.length;
+    }
+    /////////////////////////////////////////////////////////////////////////
+    public static double standardAbweichung(double[] AbweichungArray){
+        double s;
+        s= Math.sqrt(varianz(AbweichungArray));
+        return s;
+    }
+
 }
--- ProjektTaschenrechner/WachstumsUndZerfallsprozesse.java
+++ ProjektTaschenrechner/WachstumsUndZerfallsprozesse.java
@@ -1,4 +1,5 @@
 package ProjektTaschenrechner;
 
 public class WachstumsUndZerfallsprozesse {
+
 }
